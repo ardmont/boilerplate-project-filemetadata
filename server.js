6c378cbbbb32b9486c123ae6924096870ccaf0d0
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-07-16 23:52:13 -0300

    solution using express-fileupload.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var cors = require('cors');
+var fileUpload = require('express-fileupload')
 
 // require and use "multer"...
 
@@ -9,6 +10,7 @@ var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(fileUpload())
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
@@ -17,6 +19,15 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', function(req, res){
+  //res.send('teste')
+  res.json({
+      name: req.files.upfile.name,
+      type: req.files.upfile.mimetype,
+      size: req.files.upfile.size
+  })
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
